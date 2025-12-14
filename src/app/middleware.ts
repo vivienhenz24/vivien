924e0aa16fca7862f4c939739bc81df84ed5d17c
Vivien Henz <vhenz@college.harvard.edu>
2025-12-13 20:19:26 -0500

    new canary

---
+++ src/app/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import { getCanaryPayload, getCanaryHeader } from '@fuzzycanary/core'
+
+export function middleware() {
+  const payload = getCanaryPayload()
+  const { name, value } = getCanaryHeader(payload)
+  const res = NextResponse.next()
+  res.headers.set(name, value)
+  return res
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
